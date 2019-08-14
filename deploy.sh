b960a42ad77e14ef87688910e345672d83b41ca6
Elliot Charney <elliotcharney@gmail.com>
2019-08-14 12:21:17 -0700

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git init
 git add -A
 git commit -m "building for production"
 git remote add pages git@github.com:lojongstudyapp/lojongstudyapp.github.io.git
-git push pages master
+git push pages master -f
