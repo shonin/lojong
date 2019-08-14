00254a19a77f04b6158e8eb7736a065ef91a19aa
Elliot Charney <elliotcharney@gmail.com>
2019-08-14 13:55:31 -0700

    change git log message

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ npm run build
 cd ./dist
 git init
 git add -A
-git commit -m "building for production"
+git commit -m "build and force deploy for production"
 git remote add pages git@github.com:lojongstudyapp/lojongstudyapp.github.io.git
 git push pages master -f
