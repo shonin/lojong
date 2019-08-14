9ab1253dde30322a8a6a45a87a70c67110af4b1f
Elliot Charney <elliotcharney@gmail.com>
2019-08-14 08:14:39 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 npm run build
-cd /dist
+cd ./dist
 git init
 git add -A
 git commit -m "building for production"
 git remote add pages git@github.com:lojongstudyapp/lojongstudyapp.github.io.git
+git push pages master
